20e122ee9e354a91ac84e2f4e1747fa88f96061e
dewstouh <diegorodriguezpastor2@gmail.com>
2025-06-15 10:13:12 +0200

    chore: untrack .next build artefacts

--- apps/web/.next/static/chunks/apps_web_app_page_tsx_16f9f95f._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules__pnpm_a051fdd3._.js",
-  "static/chunks/apps_web_3f7e865f._.js"
-],
-    source: "dynamic"
-});
