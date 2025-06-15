d310d973cd1131264d5410fb123722d75f2dc717
dewstouh <diegorodriguezpastor2@gmail.com>
2025-06-15 20:48:22 +0200

    fix(core): link project db and remove rootDir to avoid TS6059/6307

---
+++ packages/core/src/index.ts
@@ -0,0 +1,4 @@
+export * from './services/bin/bin.service'
+export * from './services/comment/comment.service'
+export * from './services/report/report.service'
+export * from './services/user/user.service'
--- packages/core/src/services/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './report/report.service';
-export * from './user/user.service';
-export * from './bin/bin.service';
-export * from './comment/comment.service';
