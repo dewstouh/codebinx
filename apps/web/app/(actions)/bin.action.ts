593d707004e9ecb68cb20e221f860582c0463076
dewstouh <diegorodriguezpastor2@gmail.com>
2025-06-16 13:03:50 +0200

    fix(web): Only async functions are allowed to be exported in a "use server"

--- apps/web/app/(actions)/bin.action.ts
+++ apps/web/app/(actions)/bin.action.ts
@@ -6,80 +6,53 @@ import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
 import { BinService } from '@codebinx/core'
 
-export class BinAction {
-    static async create(rawData: unknown, authorClerkId: string) {
-        const parsed = parse(BinFormSchema.Create, rawData)
-        if (!parsed.success) return { success: false, issues: parsed.issues }
+export async function createBin(rawData: unknown, authorClerkId: string) {
+    const parsed = parse(BinFormSchema.Create, rawData)
+    if (!parsed.success) return { success: false, issues: parsed.issues }
 
-        try {
-            const bin = await BinService.create({
-                ...parsed.data,
-                author: { connect: { clerkUserId: authorClerkId } },
-            })
-
-            revalidatePath('/dashboard')
-            redirect(`/bin/${bin.binId}`)
-        } catch (err) {
-            console.error(err)
-            return { success: false, issues: { global: ['Error creating bin'] } }
-        }
-    }
-
-    static async update(binId: string, rawData: unknown, authorClerkId: string) {
-        const parsed = parse(BinFormSchema.Update, rawData)
-        if (!parsed.success) return { success: false, issues: parsed.issues }
-
-        try {
-            await BinService.update(binId, authorClerkId, parsed.data)
-            revalidatePath(`/bin/${binId}`)
-            return { success: true }
-        } catch (err) {
-            console.error(err)
-            return { success: false, issues: { global: ['Error updating bin'] } }
-        }
-    }
-
-    static async delete(binId: string, authorClerkId: string) {
-        try {
-            await BinService.delete(binId, authorClerkId)
-            revalidatePath('/dashboard')
-            return { success: true }
-        } catch (err) {
-            console.error(err)
-            return { success: false, issues: { global: ['Error deleting bin'] } }
-        }
-    }
-
-    static async unlock(binId: string, rawData: unknown) {
-        const parsed = parse(BinFormSchema.Unlock, rawData)
-        if (!parsed.success) return { success: false, issues: parsed.issues }
-
-        const isValid = await BinService.checkPassword(binId, parsed.data.password)
-
-        if (!isValid) {
-            return {
-                success: false,
-                issues: {
-                    password: ['Incorrect password'],
-                },
-            }
-        }
-
-        (await cookies()).set(`unlocked_bin_${binId}`, 'true', {
-            path: '/',
-            maxAge: 60 * 60 * 24 * 7, // unlocked for 7 days, should be good enough
-            httpOnly: false,
+    try {
+        const bin = await BinService.create({
+            ...parsed.data,
+            author: { connect: { clerkUserId: authorClerkId } },
         })
 
+        revalidatePath('/dashboard')
+        redirect(`/bin/${bin.binId}`)
+    } catch (err) {
+        console.error(err)
+        return { success: false, issues: { global: ['Error creating bin'] } }
+    }
+}
+
+export async function deleteBin(binId: string, authorClerkId: string) {
+    try {
+        await BinService.delete(binId, authorClerkId)
+        revalidatePath('/dashboard')
         return { success: true }
-
+    } catch (err) {
+        console.error(err)
+        return { success: false, issues: { global: ['Error deleting bin'] } }
     }
+}
 
-    static async get(binId: string) {
-        return BinService.getComplete(binId)
-    }
+export async function updateBin(binId: string, rawData: unknown, authorClerkId: string) {
+    const parsed = parse(BinFormSchema.Update, rawData)
+    if (!parsed.success) return { success: false, issues: parsed.issues }
 
-    static async getAll() {
-        return BinService.getAllPublicBinsWithAuthor()
+    try {
+        await BinService.update(binId, authorClerkId, parsed.data)
+        revalidatePath(`/bin/${binId}`)
+        return { success: true }
+    } catch (err) {
+        console.error(err)
+        return { success: false, issues: { global: ['Error updating bin'] } }
     }
+}
+
+export async function getBin(binId: string) {
+    return BinService.getComplete(binId)
+}
+
+export async function getAllPublicBins() {
+    return BinService.getAllPublicBinsWithAuthor()
 }
--- apps/web/app/(public)/_components/LatestBins.tsx
+++ apps/web/app/(public)/_components/LatestBins.tsx
@@ -4,13 +4,13 @@ import { Button } from "@/app/components/ui/button"
 import MotionWrapper from "@/app/components/MotionWrapper"
 import { Suspense } from "react"
 import BinSkeleton from "@/app/components/BinSkeleton"
-import { BinAction } from "@/app/(actions)/bin.action"
 import BinList from "@/app/components/BinList"
+import { getAllPublicBins } from "@/app/(actions)/bin.action"
 
 
 export function LatestBinsSection() {
     
-    const binResponsePromise = BinAction.getAll()
+    const binResponsePromise = getAllPublicBins()
 
     return (
         <section className="min-h-screen flex items-center justify-center py-20 px-6 lg:px-8 bg-gray-50">
